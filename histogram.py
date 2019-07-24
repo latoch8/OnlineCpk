601ca22f4a3eba7c7c0773a63ed0d6885461f92e
Pawel Latoszek <PAWEL_LATOSZEK@jabil.com>
2019-07-24 14:27:41 +0200

    added Auto refresh

--- histogram.py
+++ histogram.py
@@ -1,7 +1,6 @@
-
 import matplotlib.pyplot as plt
 import numpy as np
-import os, glob, shutil
+import os, glob, shutil, copy
 
 logsFormat = "res"
 logsDir = "../*."+logsFormat
@@ -10,27 +9,9 @@ howManyDut = 2
 delimiter = ";"
 iniDelimiter = delimiter
 testNameINI = "testsToPlot.txt"
-    
+refreshTime = 60 #w sekundach
 
-def prepareData():
-    #check and create folder
-    if not os.path.exists(tempLogsDir):
-        os.mkdir(tempLogsDir)
-        print("Directory created!")
-    else:
-        print("Directory exist!")
-    #find newes files
-    files_path = os.path.join("", logsDir)
-    files = sorted(glob.iglob(files_path), key=os.path.getctime, reverse=True) 
-    #copy newes files
-    for i in range(howManyDut):
-        # print (files[i])
-        shutil.copy(files[i],tempLogsDir)
-
-# def readAllTestName:
-#     filesList = os.listdir(tempLogsDir)
-
-def loadData():
+def loadIniFile():
     #load testNames to plote
     AllData = {}
     if not os.path.exists(testNameINI):
@@ -48,6 +29,24 @@ def loadData():
             except:
                 print("Lack of limits for: ", name)
             AllData.update({name:{"data":[], "limits":limits}})
+    return AllData
+
+def copyData():
+    #check and create folder
+    if not os.path.exists(tempLogsDir):
+        os.mkdir(tempLogsDir)
+        print("Directory created!")
+    else:
+        print("Directory exist!")
+    #find newes files
+    files_path = os.path.join("", logsDir)
+    files = sorted(glob.iglob(files_path), key=os.path.getctime, reverse=True) 
+    #copy newes files
+    for i in range(howManyDut):
+        # print (files[i])
+        shutil.copy(files[i],tempLogsDir)
+
+def loadData(AllData):
     #load data
     filesList = os.listdir(tempLogsDir)
     print(filesList)
@@ -67,33 +66,44 @@ def loadData():
         os.remove(fileDir)
     return AllData
 
-def plotAllData(data):
+def prepareCharts(data):
     howManyCharts = len(data)
-    #dzielenie wykresow
     col = howManyCharts
     row = 1
-    index = 0
     fig, plot= plt.subplots(nrows=row, ncols=col)
-    for key in data:
-        print(key)
-        a = data[key]["data"]
-        limits = data[key]["limits"]
-        lenght = len(set(a))
-        plot[index].name = key
-        plot[index].hist(a, bins = lenght)
-        #linie pionowe
-        plot[index].vlines(limits,0,1,transform=plot[index].get_xaxis_transform(), colors='r')
-        #nazwy dla osi
-        plot[index].set_title(key)
-        index += 1
     mng = plt.get_current_fig_manager()
     mng.full_screen_toggle()
-    plt.show()
+    return plot
+
+def plotData(data, plot):
+    # global fig, plot, plt
+    index = 0
+    for key in data:
+            plot[index].clear()
+            print(key)
+            a = data[key]["data"]
+            limits = data[key]["limits"]
+            lenght = len(set(a))
+            plot[index].name = key
+            plot[index].hist(a, bins = lenght)
+            #linie pionowe
+            plot[index].vlines(limits,0,1,transform=plot[index].get_xaxis_transform(), colors='r')
+            #nazwy dla osi
+            plot[index].set_title(key)
+            index += 1
+    plt.pause(refreshTime)
 
 #MAIN
-prepareData() #prepare file
-dataToPlot = loadData() #load data from file and ini
-#print(dataToPlot)
-# ani = animation.FuncAnimation(fig, animate, interval=1000)
-plotAllData(dataToPlot) #plot data
-# input("Press Enter...")
+onlyLimits = loadIniFile()
+plot = prepareCharts(onlyLimits)
+try:
+    while True:
+        copyData() #copy newes file
+        dataToPlot = copy.deepcopy(onlyLimits)
+        loadData(dataToPlot) #load data from file
+        plotData(dataToPlot, plot)
+        print(onlyLimits)
+    plt.show()
+except Exception as e:
+	print(e)
+input("Press Enter to close...")
